a56ec6740ead9e7fc1d294400a16d89bd92ccdf6
Angelina <midnightshout03@gmail.com>
2024-06-21 17:45:35 +0300

    ctrlwrapper +

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import { getContactsByIdController, getContactsController } from "../controllers/contacts.js";
+import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 
 const router = Router();
 
-router.get('/contacts', getContactsController);
-router.get('/contacts/:contactsId', getContactsByIdController);
+router.get('/contacts', ctrlWrapper(getContactsController));
+router.get('/contacts/:contactsId', ctrlWrapper(getContactsByIdController));
+
+export default router;
