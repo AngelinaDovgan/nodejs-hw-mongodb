0b5f4c2e10f120633db0b0c20c1074dcd0d7f05b
Angelina <midnightshout03@gmail.com>
2024-06-21 17:54:19 +0300

    router POST

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,4 +1,4 @@
-import { getContacts, getContactsById } from "../services/contacts.js";
+import { getContacts, getContactsById, createContact } from "../services/contacts.js";
 import createHttpError from 'http-errors';
 
 export const getContactsController = async (req, res) => {
@@ -26,4 +26,14 @@ export const getContactsByIdController = async (req, res, next) => {
         } catch (error) {
                 next(error);
             }
-    };
+};
+    
+export const createContactController = async (req, res) => {
+    const contact = await createContact(req.body);
+
+    res.status(201).json({
+        status: 201,
+        message: "Successfully created a contact!",
+        data: contact,
+    })
+};
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { getContactsByIdController, getContactsController } from "../controllers/contacts.js";
+import { getContactsByIdController, getContactsController, createContactController } from "../controllers/contacts.js";
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 
 const router = Router();
 
 router.get('/contacts', ctrlWrapper(getContactsController));
 router.get('/contacts/:contactsId', ctrlWrapper(getContactsByIdController));
+
+router.post('/contacts', ctrlWrapper(createContactController));
 
 export default router;
