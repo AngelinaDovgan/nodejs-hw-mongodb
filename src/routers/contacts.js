a4545d5a0925fef42d081d73122886afd4bc3eea
Angelina <midnightshout03@gmail.com>
2024-06-23 08:41:36 +0300

    Update contacts.js

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -5,9 +5,9 @@ import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 const router = Router();
 
 router.get('/contacts', ctrlWrapper(getContactsController));
-router.get('/contacts/:contactsId', ctrlWrapper(getContactsByIdController));
+router.get('/contacts/:contactId', ctrlWrapper(getContactsByIdController));
 
 router.post('/contacts', ctrlWrapper(createContactController));
-router.patch('/contacts/:contactsId', ctrlWrapper(patchContactController));
+router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
 
 export default router;
