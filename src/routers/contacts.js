0252f18bf79abf288aa819de010fa6baea4e7557
Angelina <midnightshout03@gmail.com>
2024-06-28 12:45:37 +0300

    validation

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,14 +1,16 @@
 import { Router } from "express";
 import { getContactsByIdController, getContactsController, createContactController, patchContactController, deleteContactController } from "../controllers/contacts.js";
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
+import { validateBody } from "../middlewares/validateBody.js";
+import { createContactSchema, updateContactSchema } from "../validation/contacts.js";
 
 const router = Router();
 
 router.get('/contacts', ctrlWrapper(getContactsController));
 router.get('/contacts/:contactId', ctrlWrapper(getContactsByIdController));
 
-router.post('/contacts', ctrlWrapper(createContactController));
-router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
+router.post('/contacts', ctrlWrapper(createContactController), validateBody(createContactSchema));
+router.patch('/contacts/:contactId', ctrlWrapper(patchContactController), validateBody(updateContactSchema));
 router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
 
 export default router;
