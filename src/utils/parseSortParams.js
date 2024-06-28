536ea1e5f0f80c7843828b1483a5b5b2ac20ca77
Angelina <midnightshout03@gmail.com>
2024-06-28 13:56:40 +0300

    add sort params + validation

---
+++ src/utils/parseSortParams.js
@@ -0,0 +1,37 @@
+import { SORT_ORDER } from "../constants";
+
+const parseSortOrder = (sortOrder) => {
+    const KnownOrder = [SORT_ORDER.ASC, SORT_ORDER.DESC].includes(sortOrder);
+    if (KnownOrder) return sortOrder;
+    return SORT_ORDER.ASC;
+};
+
+const parseSortBy = (sortBy) => {
+    const keyOfContacts = [
+        '_id',
+        'name',
+        'phoneNumber',
+        'email',
+        'isFavourite',
+        'contactType',
+        'createdAt',
+        'updatedAt',
+    ];
+    
+    if (keyOfContacts.includes(sortBy)) {
+        return sortBy;
+    }
+    return 'name';
+};
+
+export const parseSortParams = (query) => {
+    const { sortOrder, sortBy } = query;
+
+    const parsedSortOrder = parseSortOrder(sortOrder);
+    const parsedSortBy = parseSortBy(sortBy);
+
+    return {
+        sortOrder: parsedSortOrder,
+        sortBy: parsedSortBy,
+    };
+};
