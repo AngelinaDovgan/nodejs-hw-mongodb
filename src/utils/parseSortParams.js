ba11db22a7347c253c33ebd7bde03df3ea3445e1
Angelina <midnightshout03@gmail.com>
2024-06-28 14:05:58 +0300

    update path

--- src/utils/parseSortParams.js
+++ src/utils/parseSortParams.js
@@ -1,4 +1,4 @@
-import { SORT_ORDER } from "../constants";
+import { SORT_ORDER } from '../constants/index.js';
 
 const parseSortOrder = (sortOrder) => {
     const KnownOrder = [SORT_ORDER.ASC, SORT_ORDER.DESC].includes(sortOrder);
