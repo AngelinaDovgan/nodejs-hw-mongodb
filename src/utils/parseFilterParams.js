e71b6898f64b662bdccd97d8e772760e4a4ac0bf
Angelina <midnightshout03@gmail.com>
2024-06-28 15:54:50 +0300

    Update parseFilterParams.js

--- src/utils/parseFilterParams.js
+++ src/utils/parseFilterParams.js
@@ -1,21 +1,38 @@
 import { typeList } from "../constants/contacts-constants.js";
 
-const parseBoolean = value => {
-    if (typeof value !== "string") return;
-    if (!['true', 'false'].includes(value)) return;
+// const parseBoolean = value => {
+//     if (typeof value !== "string") return;
+//     if (!['true', 'false'].includes(value)) return;
 
-    const parsedValue = Boolean(value);
-    return parsedValue;
-};
+//     const parsedValue = Boolean(value);
+//     return parsedValue;
+// };
+
+const parseBoolean = (value) => {
+    if (value === 'true') return true;
+    if (value === 'false') return false;
+    return null;
+}
+
+// export const parseFilterParams = (query) => {
+//     const { type, isFavourite } = query;
+
+//     const parsedType = typeList.includes(type) ? type : null;
+//     const parsedFavourite = parseBoolean(isFavourite);
+
+//     return {
+//         type: parsedType,
+//         isFavourite: parsedFavourite,
+//     };
+// };
 
 export const parseFilterParams = (query) => {
     const { type, isFavourite } = query;
-
     const parsedType = typeList.includes(type) ? type : null;
     const parsedFavourite = parseBoolean(isFavourite);
 
     return {
         type: parsedType,
-        isFavourite: parsedFavourite,
+        isFavourite: parsedFavourite
     };
 };
