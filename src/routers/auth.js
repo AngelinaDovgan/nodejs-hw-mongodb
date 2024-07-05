4c221a9b140fdf7808fb8f4994c0c6334dd33df2
Angelina <midnightshout03@gmail.com>
2024-07-05 15:48:45 +0300

    Update auth.js

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
-import { registerUserSchema } from "../validation/auth,js";
+import { registerUserSchema } from "../validation/auth.js";
 import { loginUserController, logoutUserController, refreshUserSessionController, registerUserController } from "../controllers/auth.js";
 import { validateBody } from "../middlewares/validateBody.js";
 import { loginUserSchema } from "../validation/auth.js";
