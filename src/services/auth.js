96f51410630dd2af65b224d2b9994f42c9e73356
Angelina <midnightshout03@gmail.com>
2024-07-05 13:31:21 +0300

    Update auth.js

--- src/services/auth.js
+++ src/services/auth.js
@@ -43,3 +43,35 @@ export const loginUser = async (payload) => {
     });
 
 };
+
+const createSession = () => {
+    const accessToken = randomBytes(30).toString('base64');
+    const refreshToken = randomBytes(30).toString('base64');
+    
+    return {
+        refreshToken,
+        accessTokenValidUntil: new Date(Date.now() + FIFTEEN_MINUTES),
+        refreshTokenValidUntil: new Date(Date.now() + THIRTY_DAYS),
+    };
+};
+
+export const refreshUserSession = async ({ sessionId, refreshToken }) => {
+    const session = await SessionCollection.findOne({
+        _id: sessionId,
+        refreshToken,
+    });
+    if (!session) {
+        throw createHttpError(401, 'Session not found');
+    }
+
+    const isSessionTokenExpired = new Date() > new Date(session.refreshTokenValidUntil);
+    if (isSessionTokenExpired) {
+        throw createHttpError(401, 'Session token expired');
+    }
+
+    const newSession = createSession();
+
+    await SessionCollection.deleteOne({ _id: sessionId, refreshToken });
+
+    return
+}
