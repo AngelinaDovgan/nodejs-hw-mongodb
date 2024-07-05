9d40a641441939ff938599f442e3a6eb7553fe60
Angelina <midnightshout03@gmail.com>
2024-07-05 14:42:42 +0300

    Update auth.js

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,6 @@
 import { THIRTY_DAYS } from "../constants/index.js";
-import { loginUser, registerUser } from "../services/auth.js";
+import { loginUser, refreshUserSession, registerUser } from "../services/auth.js";
+
 
 export const registerUserController = async (req, res) => {
     const user = await registerUser(req.body);
@@ -26,6 +27,33 @@ export const loginUserController = async (req, res) => {
     res.json({
         status: 200,
         message: "Successfully logged in an user!",
+        data: {
+            accessToken: session.accessToken,
+        },
+    });
+};
+
+const setUpSession = (res, session) => {
+    res.cookie('refreshToken', session.refreshToken, {
+        httpOnly: true,
+        expires: new Date(Date.now() + THIRTY_DAYS),
+    });
+    res.cookie('sessionId', session._id, {
+        httpOnly: true,
+        expires: new Date(Date.now() + THIRTY_DAYS),
+    });
+};
+
+export const refreshUserSessionController = async (req, res) => {
+    const session = await refreshUserSession({
+        sessionId: req.cookies.sessionId,
+        refreshToken: req.cookies.refreshToken,
+    });
+    setUpSession(res, session);
+
+    res.json({
+        status: 200,
+        message: "Successfully refreshed a session!",
         data: {
             accessToken: session.accessToken,
         },
