65b56fef601737c5509a41340cf5d35352e3fff6
Angelina <midnightshout03@gmail.com>
2024-07-05 14:43:30 +0300

    Update auth.js

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 import { registerUserSchema } from "../validation/auth,js";
-import { loginUserController, registerUserController } from "../controllers/auth.js";
+import { loginUserController, refreshUserSessionController, registerUserController } from "../controllers/auth.js";
 import { validateBody } from "../middlewares/validateBody.js";
 import { loginUserSchema } from "../validation/auth.js";
 
@@ -9,5 +9,6 @@ const router = Router();
 
 router.post('/register', validateBody(registerUserSchema), ctrlWrapper(registerUserController));
 router.post('/login', validateBody(loginUserSchema), ctrlWrapper(loginUserController));
+router.post('/refresh', ctrlWrapper(refreshUserSessionController));
 
 export default router;
