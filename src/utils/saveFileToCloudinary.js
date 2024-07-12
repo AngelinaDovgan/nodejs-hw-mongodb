3621b9c12643bc397a0d7fd0918b1ef8964dc0da
Angelina <midnightshout03@gmail.com>
2024-07-12 23:03:47 +0300

    Update saveFileToCloudinary.js

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -10,6 +10,6 @@ cloudinary.v2.config({
 });
 
 export const saveFileToCloudinary = async (file) => {
-    const response = await cloudinary.v2.uploader(file.path);
+    const response = await cloudinary.v2.uploader.upload(file.path);
     return response.secure_url;
 };
