0594ec5bb91e3dae0ef550f9f7a0e4a09f099250
Angelina <midnightshout03@gmail.com>
2024-07-14 08:38:00 +0300

    Update contacts.js

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -13,7 +13,7 @@ router.use(authenticate);
 router.get('/', ctrlWrapper(getContactsController));
 router.get('/:contactId', isValidId, ctrlWrapper(getContactsByIdController));
 
-router.post('', validateBody(createContactSchema), upload.single('photo'), ctrlWrapper(createContactController));
+router.post('/', validateBody(createContactSchema), upload.single('photo'), ctrlWrapper(createContactController));
 router.patch('/:contactId', isValidId, validateBody(updateContactSchema), upload.single('photo'), ctrlWrapper(patchContactController));
 router.delete('/:contactId', isValidId, ctrlWrapper(deleteContactController));
 
