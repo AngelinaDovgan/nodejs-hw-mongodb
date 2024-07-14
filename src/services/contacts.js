f79decc3d7aa9b34e4c13174eed699223f0f1b12
Angelina <midnightshout03@gmail.com>
2024-07-14 20:38:01 +0300

    filter in service

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -53,9 +53,26 @@ export const createContact = async (payload) => {
     return contact;
 };
 
-export const updateContact = async (contactId, payload, userId, options = {}) => {
+// export const updateContact = async (contactId, payload, userId, options = {}) => {
+//     const rawResult = await ContactsCollection.findOneAndUpdate(
+//         { _id: contactId, userId },
+//         payload,
+//         {
+//             new: true,
+//             includeResultMetadata: true,
+//             ...options,
+//         },
+//     );
+//     if (!rawResult || !rawResult.value) return null;
+//     return {
+//         contact: rawResult.value,
+//         isNew: Boolean(rawResult?.lastErrorObject?.upserted),
+//     };
+// };
+
+export const updateContact = async (filter, payload, options = {}) => {
     const rawResult = await ContactsCollection.findOneAndUpdate(
-        { _id: contactId, userId },
+        filter,
         payload,
         {
             new: true,
